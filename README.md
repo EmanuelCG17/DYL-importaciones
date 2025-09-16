d48b90c3784d152482ddf2a622d6b0f17a46d0b8
EmanuelCG17 <emanuellcolorado17@gmail.com>
2025-09-16 17:16:21 -0500

    Actualización 16/09/25 a las 5:16 pm

--- README.md
+++
@@ -1,2 +0,0 @@
-# DYL-importaciones
-Proyecto en conjunto
---
+++ code.js
@@ -0,0 +1,8 @@
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+
+ const nombre=  document. getElementById('nombre')
+ const email=  document. getElementById('email')
+const password=document. getElementById('password')
+ const confirmar= document. getElementById('confirmar')
+  const form_link= document. getElementById('form_link')
